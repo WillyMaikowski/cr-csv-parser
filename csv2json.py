5694220c61fa5526daafbe819a2e2972b549d3fb
smlbiobot <smlbiobot@gmail.com>
2019-04-29 23:48:04 +0800

    Move reading to own method

--- csv2json.py
+++ csv2json.py
@@ -6,7 +6,11 @@ import os
 import re
 import sys
 
-cast = {'string': str, 'int': int, 'boolean': (lambda x: x.lower() == 'true')}
+cast = {
+    'string': str,
+    'int': int,
+    'boolean': (lambda x: x.lower() == 'true')
+}
 
 
 def camel2snake(s):
@@ -15,73 +19,79 @@ def camel2snake(s):
     return re.sub('([a-z0-9])([A-Z])', r'\1_\2', s1).lower()
 
 
+def read_csv(filepath):
+    with open(filepath, 'r', encoding='utf-8') as f:
+        rows = csv.reader(f)
+
+        line = 0
+        data = []
+
+        cols = []
+        subkey = None
+        types = []
+
+        for r in rows:
+            line += 1
+
+            if line == 1:
+                cols = camel2snake(r)
+                continue
+
+            if line == 2:
+                types = [el.lower() for el in r]
+                continue
+
+            if len(cols) != len(r):
+                print('Bad line in file ', filepath, '. Ignored line:', line)
+                continue
+
+            d = {}
+            if r[0] == '':
+                d = data.pop()
+
+            if not d:
+                subkey = 0
+            if d:
+                subkey += 1
+
+            for i in range(len(cols)):
+                if r[i] == '': continue
+
+                key = cols[i]
+                value = cast[types[i]](r[i])
+
+                if key not in d:
+                    d[key] = value
+                elif isinstance(d[key], dict):
+                    d[key][subkey] = value
+                else:
+                    aux = d[key]
+                    d[key] = {}
+                    d[key][subkey - 1] = aux
+                    d[key][subkey] = value
+
+            data.append(d)
+
+    return data
+
+
 def csv2json(filepath):
-    f = open(filepath, 'r', encoding='utf-8')
-    rows = csv.reader(f)
+    data = read_csv(filepath)
 
-    line = 0
-    data = []
-
-    cols = []
-    subkey = None
-    types = []
-
-    for r in rows:
-        line += 1
-
-        if line == 1:
-            cols = camel2snake(r)
-            continue
-
-        if line == 2:
-            types = [el.lower() for el in r]
-            continue
-
-        if len(cols) != len(r):
-            print('Bad line in file ', filepath, '. Ignored line:', line)
-            continue
-
-        d = {}
-        if r[0] == '':
-            d = data.pop()
-
-        if not d:
-            subkey = 0
-        if d:
-            subkey += 1
-
-        for i in range(len(cols)):
-            if r[i] == '': continue
-
-            key = cols[i]
-            value = cast[types[i]](r[i])
-
-            if key not in d:
-                d[key] = value
-            elif isinstance(d[key], dict):
-                d[key][subkey] = value
-            else:
-                aux = d[key]
-                d[key] = {}
-                d[key][subkey - 1] = aux
-                d[key][subkey] = value
-
-        data.append(d)
-
-    f.close()
     if len(data) <= 0:
         print('Empty file: ', filepath)
         return
 
     filepath = filepath.split('/')
-    if len(filepath) > 1: filepath.pop(0)
+    if len(filepath) > 1:
+        filepath.pop(0)
 
     newpath = os.path.join('json/', '/'.join(filepath))
     newpath = os.path.splitext(newpath)[0] + '.json'
     os.makedirs(os.path.dirname(newpath), exist_ok=True)
-    f = open(newpath, 'w')
-    f.write(json.dumps(data, indent=4))
-    f.close()
+
+    with open(newpath, 'w') as f:
+        json.dump(data, f, indent=4)
 
 
 if __name__ == '__main__':
